73f8ea03305483ae72388d942ceace0d3177fa8e
Tripo <commersonflorent@gmail.com>
2023-04-04 14:31:42 +0200

    commentaires readline

--- Jour 2/cours/exemple stream readline.js
+++ Jour 2/cours/exemple stream readline.js
@@ -1,10 +1,14 @@
 var readline = require('readline');
 
+// Création de l'interface de gestion des entrées et sorties, basée sur input et ouput de la console
 rl = readline.createInterface(process.stdin, process.stdout);
 
+// Message dans la console
 rl.setPrompt('Aloha > ');
+// définit l'invite de commande
 rl.prompt();
 
+// écoute tout les événements de la console et récupère le flux en entrée
 rl.on('line', function(line){
     switch(line.trim()) {
         case 'hello':
@@ -18,6 +22,7 @@ rl.on('line', function(line){
             break    
     }
     rl.prompt();
+     // si on fait un Ctrl + c pour arrêter le process
 }).on('close', function() {
     console.log('Have a great day !');
     process.exit(0);
