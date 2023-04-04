b5804ebb1a22283446f52702aa8de4196c397165
Tripo <commersonflorent@gmail.com>
2023-04-04 16:57:59 +0200

    Jour 2 final

--- Jour 2/Exo/mentions/index.js
+++ Jour 2/Exo/mentions/index.js
@@ -1,5 +1,16 @@
+// # 00 Exercice mentions
+
+// Installez un nouveau projet, définissez à l'aide des variables d'environnement les données suivantes :
+
+// - Une mention "assez bien" si sa moyenne est égale ou supérieure à 12/20 et inférieure à 14/20.
+// - Une mention "bien" si sa moyenne est au moins égale à 14/20 et inférieure à 16/20.
+// - Une mention "très bien" s'il obtient une moyenne égale ou supérieure à 16/20.
+
+// Utilisez les étudiants suivants et définissez les labels "Assez bien", "Bien", "Très bien" et "Passable" des mentions de chaque étudiant
+
 require('dotenv').config();
 
+//récupération des variables d'environnement
 const{app_AB, app_B, app_TB, app_P} = process.env;
 
 const students = [
@@ -18,11 +29,11 @@ const students = [
     const {note} =student;
     if (note < 10){
     student.mention = "Aucune mention";
-    }else if (note >= 10 && note <=12) {
+    }else if (note <=12) {
         student.mention = app_P;
-    }else if (note >= 12 && note <= 13) {
+    }else if (note <= 14) {
         student.mention = app_AB;
-    }else if (note >= 14 && note <= 16) {
+    }else if (note <= 16) {
         student.mention = app_B;
     }else{
         student.mention = app_TB;
