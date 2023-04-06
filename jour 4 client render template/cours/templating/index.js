cc04c861d8799f75e5b5cee5a1823899d188f8ef
Tripo <commersonflorent@gmail.com>
2023-04-06 14:41:40 +0200

    exemple pug

---
+++ jour 4 client render template/cours/templating/index.js
@@ -0,0 +1,44 @@
+const pug = require('pug');
+
+//exemple de template PUG
+
+const template = `
+if age >= 18
+    h1 Access granted !
+else
+    h1 Permission denied!`;
+
+// compile
+const compileTemplate = pug.compile(template);    
+
+const result = compileTemplate({ age : 1});
+
+// compileFile pour un fichier externe pug
+const compileTemplate2 = pug.compileFile('template.pug');
+
+compileTemplate2({ age : 19});
+
+// Render
+
+pug.render(template, {age : 19}, (err, data) => {
+    if (err) throw err;
+    console.log(data);
+})
+
+// RenderFile pour un fichier externe pug
+
+pug.renderFile('template.pug', {age : 1}, (err, data) => {
+        if (err) throw err;
+        console.log(data);
+    })
+
+//gestion erreur compile
+
+try {
+    const compileTemplate = pug.compile(template);
+    //......
+}catch (err){
+    res.writeHead(500, { 'Content-Type' : 'text/plain'});
+    res.end(err.message);
+}
+
